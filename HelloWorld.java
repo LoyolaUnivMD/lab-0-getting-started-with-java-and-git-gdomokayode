4eb4b0b80ea38d5e800da3b4ca1fc06f57f651e0
gdomokayode <156963597+gdomokayode@users.noreply.github.com>
2024-01-19 12:01:05 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,12 @@
-//HelloWorld.java
-
-class HelloWorld {
+public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-        
+        System.out.println("  GGG   OOO ");
+        System.out.println(" G   G O   O");
+        System.out.println("G      O   O");
+        System.out.println("G  GGG O   O");
+        System.out.println("G     G O   O");
+        System.out.println(" G   G O   O");
+        System.out.println("  GGG   OOO ");
     }
 }
+
