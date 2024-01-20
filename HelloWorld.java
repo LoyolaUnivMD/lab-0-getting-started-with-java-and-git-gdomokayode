0f4021c48d58af6119bf6be54f08c2b2fa9444aa
gdomokayode <156963597+gdomokayode@users.noreply.github.com>
2024-01-19 22:42:52 -0500

    Update ASCII.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,18 +4,16 @@
   // Lab Assignment: 0
   // Problem Statement: Write a Simple Java program that prints out your initial in the form of ASCII art 
   // Input: None; there is no user input in this program
-  // Output:  The initials of the programmers in ASCII art form
+  // Output:  The initials of the programmers in ASCII art form: My Initials are GO 
   // Credits: [Is your code based on an example in the book, in class, or something else? It is based from  ASCII art https://www.asciiart.eu/ also from ASCII examples from python 
   // Reminder: you should never take code from the Internet or another person. 
-    pubic class Main {
-        public static void Main (String[] args) {
-            System.out.println("  GGG   OOO ");
-            System.out.println(" G   G O   O");
-            System.out.println("G      O   O");
-            System.out.println("G  GGG O   O");
-            System.out.println("G     G O   O");
-            System.out.println(" G   G O   O");
-            System.out.println("  GGG   OOO ");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("   ____     ___");
+        System.out.println("  / ___|   / _ \\");
+        System.out.println(" | |  _|    | | |");
+        System.out.println(" | |_| |    |_| |");
+        System.out.println("  \\____|   \\___/");
     }
 }
 
